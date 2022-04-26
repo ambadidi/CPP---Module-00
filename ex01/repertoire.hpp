08e4aabf30ee6f99215ca91e60d365c541abf1bf
khmira <amine.badidi@hotmail.com>
2022-04-26 22:31:14 +0000

    class PhoneBook

---
+++ ex01/repertoire.hpp
@@ -0,0 +1,45 @@
+#ifndef REPERTOIRE_HPP
+#define REPERTOIRE_HPP
+
+# include <iomanip>
+# include <cstring>
+# include <iostream>
+# include <sstream>
+# include <string>
+class PhoneBook
+{
+private:
+    PhoneBook repertoire;
+    string      contacts[];
+
+public:
+    PhoneBook(/* args */);
+    ~PhoneBook();
+};
+
+PhoneBook::PhoneBook(/* args */)
+{
+}
+
+PhoneBook::~PhoneBook()
+{
+}
+class Contact
+{
+private:
+    /* data */
+public:
+    Contact(/* args */);
+    ~Contact();
+};
+
+Contact::Contact(/* args */)
+{
+}
+
+Contact::~Contact()
+{
+}
+
+
+#endif
