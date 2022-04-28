e41f0c39ef22b2d45ed97df9eb8ffc33f9cfee6e
khmira <amine.badidi@hotmail.com>
2022-04-28 19:28:29 +0000

    ex01

--- ex01/repertoire.hpp
+++ ex01/repertoire.hpp
@@ -6,15 +6,16 @@
 # include <iostream>
 # include <sstream>
 # include <string>
+# define MaxContact 8
 class PhoneBook
 {
 private:
-    PhoneBook repertoire;
-    string      contacts[];
+	PhoneBook	repertoire;
+	Contact		contacts[];
 
 public:
-    PhoneBook(/* args */);
-    ~PhoneBook();
+	PhoneBook(/* args */);
+	~PhoneBook();
 };
 
 PhoneBook::PhoneBook(/* args */)
@@ -27,19 +28,39 @@ PhoneBook::~PhoneBook()
 class Contact
 {
 private:
-    /* data */
+	Contact repertoire;
+	enum	enum_Entree{ADD, SEARCH, EXIT};
+	
+	string	FirstName_;
+	string	LastName_;
+	string	NickName_;
+	string	PhoneNumber_;
+	string	DarkestSecret_;
 public:
-    Contact(/* args */);
-    ~Contact();
+	void	AddContact(string FirstName_, string LastName_, string NickName_, string PhoneNumber_, string DarkestSecret_);
+	void	CheckAdd(Contact repertoire);
+//	~Contact();
 };
-
-Contact::Contact(/* args */)
+void	Contact::CheckAdd(Contact repertoire)
 {
+
 }
 
-Contact::~Contact()
+void	Contact::AddContact(string FirstName_, string LastName_, string NickName_, string PhoneNumber_, string DarkestSecret_)
 {
+	string	s1;
+	int		i = 0;
+	std::cout << "Tapez une chaine (ADD, SEARCH ou EXIT) : "; getline (cin, s1);
+	if (s1.at(i) == 'A' && s1.at(i + 1) == 'D' s1.at(i + 2) == 'D' && s1.size == 3)
+	{
+		std::cout << "Entrer Prenom (first name) "; getline (cin, FirstName_);
+	}
+	
 }
 
+/*Contact::~Contact()
+{
+}
+*/
 
 #endif
